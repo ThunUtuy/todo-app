361d0d266b1e1e3b54b9922d564ba6e397788b1e
Thun Vaniyapong <thun.vaniya@gmail.com>
2025-07-14 13:48:05 +0700

    allow display all task for a fake user as json

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 
 app = Flask(__name__)
 
@@ -10,3 +10,18 @@ def get_db_connection():
                             user=os.environ.get('DB_USERNAME'),
                             password=os.environ.get('DB_PASSWORD'))
     return conn
+
+fake_user_id = 1
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(f'SELECT * FROM tasks WHERE user_id = {fake_user_id};')
+    colnames = [desc[0] for desc in cur.description]
+    # Result: ['task_id', 'user_id', 'title', 'notes', 'due_date', 'date_added']
+    tasks = [dict(zip(colnames, row)) for row in cur.fetchall()]
+    cur.close()
+    conn.close()
+    
+    return jsonify(tasks)
